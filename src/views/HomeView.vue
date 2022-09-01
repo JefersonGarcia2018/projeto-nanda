80789b87257b3d563e6082346053f35b08a3fd4b
Jeferson Garcia - Acer 2021 <jefergarcia2013@gmail.com>
2022-09-01 02:17:00 -0400

    Feita uma alteracao basica.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,7 @@
                    <dir class="text-left">
                     <p>Com o objetivo de auxiliar os estudantes de enfermagem na consulta e entendimento sobre os Diagnósticos de Enfermagem conforme a Terminologia e Taxonomia da NANDA-I 2018-2020.</p>
                     <p>A Taxonomia da NANDA-I oferece uma maneira de classificar e categorizar áreas de preocupação de um enfermeiro (i.e., os focos dos diagnósticos).</p> 
-                    <p>Ela possui <strong>244 Diagnósticos de Enfermagem</strong>, agrupados em <strong class="text-danger">13 domínios</strong> e <strong class="text-info">47 classes</strong>.</p>
+                    <p>São <strong>244 Diagnósticos de Enfermagem</strong>, agrupados em <strong class="text-danger">13 domínios</strong> e <strong class="text-info">47 classes</strong>.</p>
                     Os 4 principais tipos de Diagnósticos são:
                     <ul>
                       <li>Diagnóstico com foco no problema</li>
